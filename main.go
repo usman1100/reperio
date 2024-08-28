aeea8e705bd220485b4256177b1606cbfc34d4c4
usman1100 <usmanahmed1100@gmail.com>
2024-08-28 16:44:29 +0500

    fix: close connection to correct host name

--- main.go
+++ main.go
@@ -8,16 +8,18 @@ import (
 
 func ScanPort(hostname string, port int) {
 	address := fmt.Sprintf("%s:%d", hostname, port)
-	_, err := net.DialTimeout("tcp", address, time.Second*5)
+	conn, err := net.DialTimeout("tcp", address, time.Second*5)
 	if err != nil {
 		fmt.Printf("Port %d  \tis closed\n", port)
 		return
 	}
+	defer conn.Close()
+
 	fmt.Printf("Port %d is open\n", port)
 }
 
 func main() {
-	hostname := "http://scanme.nmap.org"
+	hostname := "scanme.nmap.org"
 	portsToScan := [6]int{9090, 5432, 80, 443, 22, 8080}
 	for _, port := range portsToScan {
 		ScanPort(hostname, port)
